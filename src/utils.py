fd0f2813a8deac05dbe8ccc0da50b68b076cf0f1
shyyhs <songskg6666@gmail.com>
2018-11-28 15:24:33 +0900

    auto Ja-En pair for one url

--- src/utils.py
+++ src/utils.py
@@ -1,40 +1,69 @@
 # -*- coding: utf-8 -*-
 from globalSetting import *
 
+
 def cpJa(url): return "http://ja.wikipedia.org"+url
 def cpEn(url): return "http://en.wikipedia.org"+url
+def urlType(soup):
+    return "Others"
 
-# Return a dict{"title":"url"}
-# Return the title and english translation
-def urlLst(url):
+unicodeType = type(u"例")
+
+# regular expression patterns
+urlEngPattern = re.compile(ur"英.*[:：][ ]*([a-zA-z0-9 ]+)[)）]",re.UNICODE)
+engPattern = re.compile(ur"英.*[:：][ ]*([a-zA-z0-9 ]+)$", re.UNICODE)
+
+# Input: soup,
+# Output: The english translation of the title of the soup if exists else "NONE"
+def wikiFindEngWordFromText(soup):
+    enWord = "NONE"
+    result = urlEngPattern.search(soup.get_text())
+    if (result is not None):
+        enWord = result.group(1).strip()
+        return enWord
+    return enWord
+
+def wikiFindEngWordFromUrl(soup):
+    enWord = "NONE"
+    for link in soup.find_all('a'):
+        if (link.get_text() == "English"):
+            result = engPattern.search(link.get("title"))
+            if (result is not None):
+                enWord = result.group(1).strip()
+                return enWord
+    return enWord
+
+def wikiEnWord(soup):
+    enWord = wikiFindEngWordFromText(soup)
+    if (enWord == "NONE"):
+        enWord = wikiFindEngWordFromUrl(soup)
+    return enWord
+
+# Input: soup, Output: Wiki Title
+def webTitle(soup): return soup.find("h1").get_text()
+
+# Input:soup, Output: dict{"title":"url"}
+def wikiUrlDic(soup):
+    urlDic = {}
+    for link in soup.find_all('a',href=re.compile("^/wiki/((?!:).)*$")):
+        urlDic[link.get('title')] = cpJa(link.get('href'))
+    return urlDic
+
+def cookSoup(url):
     html = requests.get(url).text
     soup = sp(html,"lxml")
-    #1. the urlDic
-    urlDic = {}
-    for href in soup.find_all('a',href=re.compile("^/wiki/((?!:).)*$")):
-        urlDic[href.get('title')] = cpJa(href.get('href'))
-    #2. the title 
-    jaWord = soup.find("h1").get_text()
-    #3. english translation if any
-    unicodeType = type(u"例")
-    pattern = re.compile(ur"英.*[:：]([a-zA-z0-9 ]+)[)）]",re.UNICODE)
-    abstract = soup.find("div", {"id":"mw-content-text"}).find_all("p")
-    for sentence in abstract:
-        sentText = sentence.get_text()
-        if (type(sentText)!=unicodeType):
-            sentText= unicode(sentText,'utf-8')
-        result = pattern.search(sentText)
-        if (result is not None):
-            enWord = result.group(1).strip()
-            break;
-    """
-    #caption = abstract.find("caption", itemprop="name")
-    st = "英: Japan Freight Railway Company）"
-    st = unicode(st, 'utf-8')
-    result = pattern.search(st)
-    if (result is not None): print (result.group(1).strip())
-    """
-    return urlDic, jaWord, enWord
+    return soup
+
+def crawlEntry(sourceUrl):
+    soup = cookSoup(sourceUrl)
+    urlDic = wikiUrlDic(soup)
+    jaWord = webTitle(soup)
+    enWord = wikiEnWord(soup)
+    print (jaWord, enWord)
+
+
+
+
 
     
 
@@ -42,8 +71,8 @@ def urlLst(url):
 if (__name__=="__main__"):
     print ("utils begins")
     url = "https://ja.wikipedia.org/wiki/%E4%BA%AC%E9%83%BD%E6%B0%B4%E6%97%8F%E9%A4%A8"
-    urlDic, jaWord, enWord = urlLst(url)
-    print (jaWord, enWord)
+    url = "https://ja.wikipedia.org/wiki/%E6%A0%AA%E5%BC%8F%E4%BC%9A%E7%A4%BE_(%E6%97%A5%E6%9C%AC)"
+    crawlEntry(url)
 
 if (__name__!="__main__"):
     print ("utils loaded")
